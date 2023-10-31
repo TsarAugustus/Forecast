34cbe48bc2b741b12aa2679985addd65b86fe193
N/A <ostap.john@gmail.com>
2023-10-31 15:43:47 -0700

    This actually kind of works. Please work.

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ let inspectionLimits = {
 		'I': 5,
 		'K': 1,
 		'P': 5,
-		'T': undefined,
+		'T': 2,
 		'L': undefined,
 		'UC': 5,
 		'WF' : 5
@@ -93,352 +93,331 @@ let months = [
 	'December'
 ];
 
+let companies = [];
+
 app.get('/forecast', (req, res) => {	
 	const workbook = XLSX.readFile('./uploads/log.xlsx');
 	const sheetNameList = workbook.SheetNames;
-	console.log('FORECAST')
-	
-	// let newControlSheet = sheetNameList[0];
-	// let propaneSeason = sheetNameList[1];
-	// let oldControlSheet = sheetNameList[2];
+	console.log('FORECAST');
 	
 	let newControlSheetResult = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNameList[0]]);
 	let oldControlSheetResult = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNameList[2]]);
+	
+	let oldInfo = getOldWorkbookInformation(oldControlSheetResult);
+	
+	let newInfo = getNewWorkbookInformation(newControlSheetResult);
+	
+	// console.log(companies[0].units.find(unit => unit.name === 'T13'))
 
-	let data = [];
-
-	// let currentMonth = new Date().getMonth() + 1;
-
-	months.forEach(month => {
-		let monthInspections = {
-			month: month,
-			companies: []
-			
-		};
-
-		data.push(monthInspections);
-	});
-
-	let oldInfo = getOldControlSheetInformation(oldControlSheetResult);
-
-	let newInfo = getNewControlSheetInformation(newControlSheetResult, oldInfo);
-
-	let calendar = assembleCalendar(newInfo);
-
-	let x = newInfo.find(company => company.CUSTOMER === 'Scamp')
-
-	// console.log(x)
-
-
-	// console.log(calendar)
+	let calendar = assembleCalendar(companies)
+	
 	res.render('forecast', { title: 'Forecast', calendar: calendar} );
 });
-
-function assembleCalendar(info) {
-	let calendar = [];
-	info.forEach(customer => {
-		customer.units.forEach(unit => {
-
-			if(unit.UNIT === '312AB') {
-				// console.log(unit)
-			}
-			
-			for(let inspection in unit.INSPECTIONS) {
-				let thisInspection = unit.INSPECTIONS[inspection];
-				
-				if(thisInspection !== undefined) {
-					let findYear = calendar.find(item => item.year === thisInspection.year)
-
-					if(!findYear) {
-						let newYear = {
-							year: thisInspection.year,
-							months: []
-						}
-
-						if(newYear.year) calendar.push(newYear);
-					}
-
-					if(findYear) {
-						let findMonthInYear = findYear.months.find(item => item.month === thisInspection.month)
-						if(!findMonthInYear) {
-							let newMonth = {
-								name: months[thisInspection.month],
-								month: thisInspection.month,
-								customers: []
-							}
-
-							findYear.months.push(newMonth);
-						}
-
-						
-						if(findMonthInYear) {
-							let findCustomerInMonth = findMonthInYear.customers.find(item => item.CUSTOMER === customer.CUSTOMER);
-
-							if(!findCustomerInMonth) {
-								let newMonthCustomer = {
-									CUSTOMER: customer.CUSTOMER,
-									units: []
-								}
-
-								findMonthInYear.customers.push(newMonthCustomer)
-							}
-							
-							if(findCustomerInMonth) {
-								let findUnitInCustomerMonth = findCustomerInMonth.units.find(item => item.UNIT === customer.UNIT);
-
-								if(!findUnitInCustomerMonth) {
-									let newUnit = {
-										UNIT: unit.UNIT.toString().replace(/[^\w.]+/g, ""),
-										inspection: ''
-									}
-
-									for(let inspection in unit.INSPECTIONS) {
-										// let inspectionYear = undefined;
-
-										if(unit.INSPECTIONS[inspection] !== undefined) {
-											if(unit.INSPECTIONS[inspection].year === findYear.year) {
-												newUnit.inspection = newUnit.inspection + inspection
-											}
-										}
-
-									}
-
-									findCustomerInMonth.units.push(newUnit);
-								}
-							}
-						}
-					}
-				}
-			}
-		})
-	});
-
-	calendar.sort(function(a, b) {
-		return a.year - b.year;
-	});
-	
-	calendar.forEach(year => {
-		year.months.sort(function(a, b) {
-			return a.month - b.month
-		})
-
-	});
-
-	calendar.forEach((year, index) => {
-		if(year.year < new Date().getFullYear()) {
-			calendar.splice(index, 1)
-		}
-	})
-
-	return calendar;
-}
 
 function ExcelDateToJSDate(serial) {
 	return new Date((serial - (25567 + 1))*86400*1000);
 }
 
-function getNewControlSheetInformation(sheet, oldSheet) {
-	let newControlSheet = oldSheet;
+function assembleCalendar(info) {
+	let calendar = [];
 
-	sheet.forEach(newItem => {
-		
-		if(!newControlSheet.find(item => item.CUSTOMER === newItem.CUSTOMER)) {
-			let newCompany = {
-				CUSTOMER: newItem.CUSTOMER,
-				units: []
-			}
+	info.forEach(company => {
+		company.units.forEach(unit => {
+			for(let inspection in unit.inspections) {
+				let thisInspection = unit.inspections[inspection];
 
-			newControlSheet.push(newCompany);
-		}
-
-		let companyInSheet = newControlSheet.find(item => item.CUSTOMER === newItem.CUSTOMER);
-		
-		if(companyInSheet !== undefined) {
-			
-			if(!companyInSheet.units.find(unit => unit.UNIT === newItem.UNIT) && newItem.UNIT !== 'UNIT') {
-				let newUnit = {
-					UNIT: newItem.UNIT,
-					INSPECTIONS: {
-						'V': undefined,
-						'I': undefined,
-						'K': undefined,
-						'P': undefined,
-						'T': undefined,
-						'L': undefined,
-						'UC': undefined,
-						'WF' : undefined
+				if(!calendar.find(year => year.year === unit.inspections[inspection].year) && unit.inspections[inspection].year !== 0 && !isNaN(unit.inspections[inspection].year)) {
+					let newYear = {
+						year: unit.inspections[inspection].year,
+						months: []
 					}
+
+					if(unit.inspections[inspection].year === isNaN(unit.inspections[inspection].year) || unit.inspections[inspection].year === 0) {
+						console.log(company, unit)
+					}
+
+					months.forEach((month, index) => {
+						let newMonth = {
+							month: month,
+							index: index,
+							customers: []
+						}
+
+						newYear.months.push(newMonth)
+					})
+
+					calendar.push(newYear)
 				}
-
-				companyInSheet.units.push(newUnit);
 			}
+		})
+	})
 
-			let unitInCompanySheet = companyInSheet.units.find(unit => unit.UNIT === newItem.UNIT && newItem.UNIT !== 'UNIT');
-
-			if(unitInCompanySheet !== undefined) {
+	info.forEach(company => {
+		company.units.forEach(unit => {
+			for(let inspection in unit.inspections) {
+				let inspectionYear = unit.inspections[inspection].year;
+				let inspectionMonth = unit.inspections[inspection].month;
 				
-			}
-			
-			for(let letter in newItem) {
-				let thisInspection = undefined;
+				let yearInCalendar = calendar.find(year => year.year === inspectionYear);
+				
+				if(yearInCalendar !== undefined) {
+					let monthInCalendar = yearInCalendar.months.find(month => month.index === inspectionMonth);
 
-				// ==========================
-				if(letter === 'Visual' && newItem[letter] !== 'N/A') {
-					thisInspection = 'V';
-				}
+					if(monthInCalendar !== undefined) {
+						let customerInMonth = monthInCalendar.customers.find(customer => customer.name === company.name)
 
-				if(letter === 'Internal' && newItem[letter] !== 'N/A') {
-					thisInspection = 'I';
-				}
+						if(!customerInMonth) {
+							let newCustomer = {
+								name: company.name,
+								units: []
+							}
 
-				if(letter === 'Upper Coupler' && newItem[letter] !== 'N/A') {
-					thisInspection = 'UC';
-				}
-
-				if(letter === 'Lining' && newItem[letter] !== 'N/A') {
-					thisInspection = 'L';
-				}
-
-				if(letter === 'Wet Fluorescent' && newItem[letter] !== 'N/A') {
-					thisInspection = 'WF';
-				}
-
-				if(letter === 'Leakage' && newItem[letter] !== 'N/A') {
-					thisInspection = 'K';
-				}
-
-				if(letter === 'Pressure' && newItem[letter] !== 'N/A') {
-					thisInspection = 'P';
-				}
-
-				if(letter === 'Thickness' && newItem[letter] !== 'N/A') {
-					thisInspection = 'T';
-				}
-				// ==========================
-
-				if(thisInspection !== undefined) {
-					
-					let unitMonth = ExcelDateToJSDate(newItem.DATE).getMonth();
-					let unitYear = ExcelDateToJSDate(newItem.DATE).getFullYear();
-					let spec = undefined;
-					if(newItem['TANK SPEC'] !== 'HOSES') {
-						spec = newItem['TANK SPEC'].match(/(\d+)/)[0]
+							monthInCalendar.customers.push(newCustomer)
+						} 					
 					}
-
-					// let unitInCompanySheet = companyInSheet.units.find(unit => unit.UNIT === newItem.UNIT);
-
-					// console.log(inspectionLimits['406'][thisInspection])
-
-					if(spec === '306' || spec === '406') {
-						// console.log('406')
-						unitInCompanySheet.INSPECTIONS[thisInspection] = {
-							month: unitMonth,
-							year: unitYear + inspectionLimits['406'][thisInspection]
-						}
-					}
-
-					if(spec === '307' || spec === '407') {
-						// console.log('407')
-						unitInCompanySheet.INSPECTIONS[thisInspection] = {
-							month: unitMonth,
-							year: unitYear + inspectionLimits['407'][thisInspection]
-						}
-					}
-
-					if(spec === '330' || spec === '331') {
-						// console.log('331')
-						unitInCompanySheet.INSPECTIONS[thisInspection] = {
-							month: unitMonth,
-							year: unitYear + inspectionLimits['331'][thisInspection]
-						}
-					}
-					
 				}
 			}
+		})
+	})
 
-		}
+	info.forEach(company => {
+		company.units.forEach(unit => {
+			for(let inspection in unit.inspections) {
+				let inspectionYear = unit.inspections[inspection].year;
+				let inspectionMonth = unit.inspections[inspection].month;
+				
+				let yearInCalendar = calendar.find(year => year.year === inspectionYear);
+
+				// if(yearInCalendar === undefined) console.log('here', unit)
+				if(yearInCalendar !== undefined) {
+					let monthInCalendar = yearInCalendar.months.find(month => month.index === inspectionMonth);
+
+					let customerInMonth = monthInCalendar.customers.find(customer => customer.name === company.name)
+
+					let unitInCustomerMonth = customerInMonth.units.find(thisUnit => thisUnit.name === unit.name)
+
+					if(!unitInCustomerMonth) {
+						let newUnit = {
+							name: unit.name,
+							inspection: ''
+						}
+
+						for(let inspection in unit.inspections) {
+							if(unit.inspections[inspection].year === yearInCalendar.year && monthInCalendar.index === unit.inspections[inspection].month) {
+								newUnit.inspection += inspection
+							}
+						}
+
+						customerInMonth.units.push(newUnit)
+					}
+				}
+			}
+		})
+	})
+
+	calendar.sort(function(a, b) {
+		return a.year - b.year;
 	});
 
-	return newControlSheet;
+	calendar.forEach(year => {
+		year.months.sort(function(a, b) {
+			return a.index - b.index
+		})
+
+	});
+	
+	// console.log(calendar[2].months[9].customers[2])
+
+	return calendar;
 }
 
-function getOldControlSheetInformation(sheet) {
-	let oldControlSheet = [];
+function getOldWorkbookInformation(sheet) {
 
-	sheet.forEach(oldItem => {
-		let companyInSheet = oldControlSheet.find(item => item.CUSTOMER === oldItem.CUSTOMER);
+	//Get all customers
+	sheet.forEach(item => {
+		let findCompany = companies.find(company => company.name === item.CUSTOMER)
 
-		if(!companyInSheet) {
+		if(findCompany === undefined && item.CUSTOMER !== undefined) {
 			let newCompany = {
-				CUSTOMER: oldItem.CUSTOMER,
+				name: item.CUSTOMER,
 				units: []
 			}
 
-			if(newCompany.CUSTOMER !== undefined) oldControlSheet.push(newCompany)
-		}
-
-		if(companyInSheet !== undefined) {
-			
-			if(!companyInSheet.units.find(unit => unit.UNIT === oldItem.UNIT) && oldItem.UNIT !== 'UNIT') {
-				let newUnit = {
-					UNIT: oldItem.UNIT,
-					INSPECTIONS: {
-						'V': undefined,
-						'I': undefined,
-						'K': undefined,
-						'P': undefined,
-						'T': undefined,
-						'L': undefined,
-						'UC': undefined,
-						'WF' : undefined
-					}
-				}
-
-				companyInSheet.units.push(newUnit)
-			}
-
-			for(let letter of oldItem.INSPECTION) {
-				let unitInCompanySheet = companyInSheet.units.find(unit => unit.UNIT === oldItem.UNIT);
-
-				if(letter === 'U' || letter === 'C') {
-					letter = 'UC'
-				}
-
-				let unitMonth = ExcelDateToJSDate(oldItem.DATE).getMonth();
-				let unitYear = ExcelDateToJSDate(oldItem.DATE).getFullYear();
-
-				if(!unitMonth && unitMonth !== 0 && oldItem.UNIT !== 'UNIT') {
-					let newMonth = oldItem.DATE.split('-')[0];
-					let newYear = oldItem.DATE.split('-')[2];
-
-					unitMonth = Number(newMonth);
-					unitYear = Number(newYear);
-				}
-
-				if(oldItem['TANK SPEC'] === 306 || oldItem['TANK SPEC'] === 406) {
-					unitInCompanySheet.INSPECTIONS[letter] = {
-						month: unitMonth,
-						year: unitYear + inspectionLimits['406'][letter]
-					}
-				}
-
-				if(oldItem['TANK SPEC'] === 307 || oldItem['TANK SPEC'] === 407) {
-					unitInCompanySheet.INSPECTIONS[letter] = {
-						month: unitMonth,
-						year: unitYear + inspectionLimits['407'][letter]
-					}
-				}
-
-				if(oldItem['TANK SPEC'] === 330 || oldItem['TANK SPEC'] === 331) {
-					unitInCompanySheet.INSPECTIONS[letter] = {
-						month: unitMonth,
-						year: unitYear + inspectionLimits['331'][letter]
-					}
-				}
-			}
-
+			companies.push(newCompany)
 		}
 	});
 
-	return oldControlSheet;
+	sheet.forEach(item => {
+		let findCompany = companies.find(company => company.name === item.CUSTOMER)
+
+		if(item.UNIT !== undefined) {
+			let findUnitInCompany = findCompany.units.find(unit => unit.name.toString() === item.UNIT.toString())
+
+			if(findUnitInCompany === undefined) {
+				let newUnit = {
+					name: item.UNIT.toString(),
+					inspections: {
+						'V': {month: 0, year: 0},
+						'I': {month: 0, year: 0},
+						'K': {month: 0, year: 0},
+						'P': {month: 0, year: 0},
+						'T': {month: 0, year: 0},
+						'L': {month: 0, year: 0},
+						'UC': {month: 0, year: 0},
+						'WF' : {month: 0, year: 0}
+					}
+				}
+
+				findCompany.units.push(newUnit)
+			}
+		}
+
+	});
+
+	sheet.forEach(item => {
+		let findCompany = companies.find(company => company.name === item.CUSTOMER);
+
+		if(item.UNIT !== undefined && item.UNIT !== 'UNIT') {
+			let findUnitInCompany = findCompany.units.find(unit => unit.name.toString() === item.UNIT.toString());
+
+			let unitMonth = ExcelDateToJSDate(item.DATE).getMonth();
+			let unitYear = ExcelDateToJSDate(item.DATE).getFullYear();
+
+			if(!unitMonth && unitMonth !== 0 && item.UNIT !== 'UNIT') {
+				let newMonth = item.DATE.split('-')[0];
+				let newYear = item.DATE.split('-')[2];
+
+				unitMonth = Number(newMonth);
+				unitYear = Number(newYear);
+			}
+
+			for(let letter in item.INSPECTION) {
+				inspectionLetter = item.INSPECTION[letter];
+				if(inspectionLetter === '/') {
+					continue;
+				} else if(inspectionLetter === 'U' || inspectionLetter === 'C') {
+					inspectionLetter = 'UC';
+				} else if(inspectionLetter === 'W' || inspectionLetter === 'F') {
+					inspectionLetter = 'WF';
+				} else if(findUnitInCompany.inspections[inspectionLetter] === undefined) {
+					continue
+				}
+
+				// console.log(findUnitInCompany.inspections[inspectionLetter], inspectionLetter, 'mid', findUnitInCompany)
+				findUnitInCompany.inspections[inspectionLetter].month = unitMonth;
+				
+
+				let spec = item['TANK SPEC'];
+				// console.log(spec)
+				if(spec === 306 || spec === 406) {
+					findUnitInCompany.inspections[inspectionLetter].year = unitYear + inspectionLimits['406'][inspectionLetter];
+				} else if (spec === 307 || spec === 407) {
+					findUnitInCompany.inspections[inspectionLetter].year = unitYear + inspectionLimits['407'][inspectionLetter];
+				} else if (spec === 330 || spec === 331) {
+					findUnitInCompany.inspections[inspectionLetter].year = unitYear + inspectionLimits['331'][inspectionLetter];
+				}
+
+			}
+		}
+	
+	})
+
+	// companies.forEach(company => {
+	// 	if(company.name === 'Ken Johnson') {
+	// 		company.units.forEach(unit => {
+	// 			if(unit.name === 'T12') {
+	// 				console.log(unit)
+	// 			}
+	// 		})
+	// 	}
+	// })
+
+}
+
+function getNewWorkbookInformation(sheet) {
+	sheet.forEach(item => {
+		let findCompany = companies.find(company => company.name === item.CUSTOMER);
+	
+		if(findCompany === undefined && item.CUSTOMER !== undefined) {
+			let newCompany = {
+				name: item.CUSTOMER,
+				units: []
+			}
+
+			companies.push(newCompany)
+		}
+	});
+
+	sheet.forEach(item => {
+		let findCompany = companies.find(company => company.name === item.CUSTOMER)
+
+		if(item.UNIT !== undefined) {
+			let findUnitInCompany = findCompany.units.find(unit => unit.name.toString() === item.UNIT.toString())
+
+			if(findUnitInCompany === undefined) {
+				let newUnit = {
+					name: item.UNIT.toString(),
+					inspections: {
+						'V': {month: 0, year: 0},
+						'I': {month: 0, year: 0},
+						'K': {month: 0, year: 0},
+						'P': {month: 0, year: 0},
+						'T': {month: 0, year: 0},
+						'L': {month: 0, year: 0},
+						'UC': {month: 0, year: 0},
+						'WF' : {month: 0, year: 0}
+					}
+				}
+
+				findCompany.units.push(newUnit)
+			}
+		}
+
+	});
+
+	sheet.forEach(item => {
+		let findCompany = companies.find(company => company.name === item.CUSTOMER);
+
+		for(let obj in item) {
+			let findUnitInCompany = findCompany.units.find(unit => unit.name.toString() === item.UNIT.toString());
+			let inspectionLetter = undefined;
+			
+			if(obj === 'Visual') inspectionLetter = 'V'
+			if(obj === 'Internal') inspectionLetter = 'I'
+			if(obj === 'Leakage') inspectionLetter = 'K'
+			if(obj === 'Pressure') inspectionLetter = 'P'
+			if(obj === 'Thickness') inspectionLetter = 'T'
+			if(obj === 'Lining') inspectionLetter = 'L'
+			if(obj === 'UC') inspectionLetter = 'UC'
+			if(obj === 'WF') inspectionLetter = 'WF'
+
+			let unitMonth = ExcelDateToJSDate(item.DATE).getMonth();
+			let unitYear = ExcelDateToJSDate(item.DATE).getFullYear();
+
+			if(inspectionLetter && item[obj] !== 'N/A') {
+				findUnitInCompany.inspections[inspectionLetter].month = unitMonth;
+
+				let spec = item['TANK SPEC'].replace(/\D/g,'');
+				
+				if(spec === '306' || spec === '406') {
+					findUnitInCompany.inspections[inspectionLetter].year = unitYear + inspectionLimits['406'][inspectionLetter];
+				} else if (spec === '307' || spec === '407') {
+					findUnitInCompany.inspections[inspectionLetter].year = unitYear + inspectionLimits['407'][inspectionLetter];
+				} else if (spec === '330' || spec === '331') {
+					findUnitInCompany.inspections[inspectionLetter].year = unitYear + inspectionLimits['331'][inspectionLetter];
+				}
+
+				if(isNaN(unitYear)) {
+					console.log('HERE')
+				}
+
+				if(isNaN(findUnitInCompany.inspections[inspectionLetter].year)) {
+					console.log('HMMMMM', item, ExcelDateToJSDate(item.DATE).getFullYear(), inspectionLetter)
+				}
+
+				// console.log(findUnitInCompany, `LETTER: ${inspectionLetter}`, unitYear)
+			}
+
+		}
+	})
 }
